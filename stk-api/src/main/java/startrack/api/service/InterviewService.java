4e9b4c47f8d95b9fa0c82349dde153798d35acc1
Julio Faerman <jfaerman@gmail.com>
2022-03-08 22:45:20 +0100

    Initial home page

--- stk-api/src/main/java/startrack/api/service/InterviewService.java
+++ stk-api/src/main/java/startrack/api/service/InterviewService.java
@@ -1,6 +1,7 @@
 package startrack.api.service;
 
 import startrack.api.entity.InterviewEventEntity;
+import startrack.api.entity.TenantEntity;
 import startrack.model.InterviewEvent;
 
 import javax.enterprise.context.Dependent;
@@ -8,7 +9,8 @@ import javax.enterprise.context.Dependent;
 @Dependent
 public class InterviewService extends Service{
 
-    public InterviewEvent findByUUID(String uuid) {
+    public InterviewEvent findByUUID(TenantEntity tenant, String uuid) {
+        //TODO: Check tentant
         return findByUUID(InterviewEventEntity.class, uuid)
                 .map(InterviewEventEntity::toInterviewEvent)
                 .get();
